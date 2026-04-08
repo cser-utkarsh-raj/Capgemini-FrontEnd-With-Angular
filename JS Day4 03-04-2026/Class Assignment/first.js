e3fe07d7da728ef9b0c81debf2686907480e3360
cser-utkarsh-raj <cser.utkarsh.raj@gmail.com>
2026-04-08 10:52:46 +0530

    Updated Day 4

---
+++ JS Day4 03-04-2026/Class Assignment/first.js
@@ -0,0 +1,11 @@
+let user = {
+    name: "",
+    age: 0,
+    city: "",
+    profession: "",
+    hobbies: [],
+
+    greet: function() {
+        console.log("Hello, my name is " + this.name);
+    }
+}; 
---
+++ JS Day4 03-04-2026/Class Assignment/second.js
@@ -0,0 +1,22 @@
+// function startTimer() {
+//     let count = 0;
+
+//     // Runs every 1 second
+//     let intervalId = setInterval(function () {
+//         count++;
+//         console.log("Count:", count);
+//     }, 1000);
+
+//     // Stops after 5 seconds
+//     setTimeout(function () {
+//         clearInterval(intervalId);
+//         console.log("Timer stopped!");
+//     }, 5000);
+// }
+
+// // Call the function
+// startTimer();
+
+async function getData() {o                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    
+}
